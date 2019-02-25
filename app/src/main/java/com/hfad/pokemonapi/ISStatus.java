31aa65a88b3abcfc542d3244997923e37f268435
Lim Bui <limxbui@gmail.com>
2019-02-25 09:09:48 -0800

    Initial commit

---
+++ app/src/main/java/com/hfad/pokemonapi/ISStatus.java
@@ -0,0 +1,9 @@
+package com.hfad.pokemonapi;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class ISStatus {
+    private LatLng issposition =
+    private String message;
+    private int timestamp;
+}
